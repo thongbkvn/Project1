0af16af388f6b6d14308a895bb443e3df073824a
Pham Van Thong <thongbkvn@gmail.com>
2015-01-24 10:47:18 +0700

    Merge Sort

--- src/bai1/bai1.cpp
+++ src/bai1/bai1.cpp
@@ -1,24 +1,26 @@
 /*
   Doi tuong: Day so
   Thuoc tinh: 
-              -Chieu dai.
-              -Luu tru: Cap phat dong
+  -Chieu dai.
+  -Luu tru: Cap phat dong
   Phuong thuc:
-              -Nhap day so
-	          +Tu ban phim
-		  +Tu file
-	      -Xuat day so
-	          +Tu ban phim
-		  +Tu file
-	      -Sap xep buble, heap, quick
+  -Nhap day so
+  +Tu ban phim
+  +Tu file
+  -Xuat day so
+  +Tu ban phim
+  +Tu file
+  -Sap xep buble, heap, quick
 */
 
 
 #include <iostream>
-#include <fstream>
+#include <fstream> //Su dung nhap xuat file
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define RANGE 20
+#define INF 4294967295
 using namespace std;
 
 class dayso
@@ -27,27 +29,37 @@ private:
   int len;
   int *p;
 public:
+  int bublesort();
+
+  void merge(int dau, int giua, int duoi);
+  void mergesort(int dau, int duoi)
+  {
+    int giua;
+    if (dau<duoi)
+      {
+	giua=(dau+duoi)/2;
+	mergesort(dau,giua);
+	mergesort(giua+1,duoi);
+	merge(dau,giua,duoi);
+      }
+  }
+  
+  //cap phat bo nho
   void capphat(int n)
   {
     p=new int[n];
     len=n;
   }
-  int bublesort()
+
+  //hoan vi p[m] va p[n]
+  void hoanvi(int m,int n)
   {
-    int i,j,s=0;
-    for (i=0;i<len;i++)
-      for (j=i+1;j<len;j++)
-	{
-	  if (p[i]>p[j])
-	    {
-	      p[i]=p[i]+p[j];
-	      p[j]=p[i]-p[j];
-	      p[i]=p[i]-p[j];
-	      s++;
-	    }
-	}
-    return s;
+    p[m]=p[m]+p[n];
+    p[n]=p[m]-p[n];
+    p[m]=p[m]-p[n];
   }
+
+  //Cac ham nhap file
   void nhapbp()
   {
     int i, n;
@@ -67,18 +79,22 @@ public:
     for (i=0;i<n;i++)
       inp>>p[i];
   }
+
+  //Cac ham xuat file
   void xuatmh()
   {
     int i;
     cout<<"\nXuat day so:"<<endl;
     for (i=0;i<len;i++)
       {
-	  cout<<p[i]<<'\t';
-	  if (i%10==0)
-	    cout<<endl;
+	cout<<p[i]<<'\t';
+	if (i%10==9)
+	  cout<<endl;
       }
     cout<<endl;
   }
+
+  
   void xuatfile(const char* filename="bai1.out")
   {
     ofstream outp(filename);
@@ -95,31 +111,96 @@ public:
   }
 };
 
-void taodt(int n,const char* filename="bai1.in")
+//Sap xep noi bot
+int dayso::bublesort()
 {
-  int i;
+  int i,j,s=0;
+  for (i=0;i<len;i++)
+    for (j=i+1;j<len;j++)
+      if (p[i]>p[j])
+	{
+	  hoanvi(i,j);
+	  s++;
+	}
+  return s;
+}
+
+/* Sap xep tron
+  
+ */
+
+
+void dayso::merge(int dau,int giua,int duoi)
+{
+
+  int i,m,k,l,temp[100];
+
+  l=dau;
+  i=dau;
+  m=giua+1;
+
+  while((l<=giua)&&(m<=duoi)){
+
+    if(p[l]<=p[m]){
+      temp[i]=p[l];
+      l++;
+    }
+    else{
+      temp[i]=p[m];
+      m++;
+    }
+    i++;
+  }
+
+  if(l>giua){
+    for(k=m;k<=duoi;k++){
+      temp[i]=p[k];
+      i++;
+    }
+  }
+  else{
+    for(k=l;k<=giua;k++){
+      temp[i]=p[k];
+      i++;
+    }
+  }
+   
+  for(k=dau;k<=duoi;k++){
+    p[k]=temp[k];
+  }
+}
+
+
+//Tao dau vao
+void taodulieu(int n,const char* filename="bai1.in")
+{
+  int i,a;
   ofstream outfile(filename);
   outfile<<n<<endl;
+  srand(time(NULL));
   for (i=0;i<n;i++)
     {
-	  outfile<<(rand()%20000-10000)<<'\t';
-	  if (i%10 ==9)
-	    outfile<<endl;
+      a=rand()%RANGE;
+      outfile<<a<<'\t';
+      if (i%10 ==9)
+	outfile<<endl;
     }
   outfile<<endl;
   outfile.close();
 }
+
 int main(void)
 {
   dayso list;
   int a;
-  taodt(50000);
+  taodulieu(30);
   cout<<"\nNhap du lieu";
   list.nhapfile();
-  cout<<"\nSap xep..."<<endl;
-  a=list.bublesort();
-  cout<<"\nSap xep hoan thanh"<<endl;
-  list.xuatfile();
-  cout<<"\nSo lan trao doi: "<<a<<endl;
+  list.xuatmh();
+  list.mergesort(0,30);
+  list.xuatmh();
+
+
+  cout<<endl;
   return 0;
 }
